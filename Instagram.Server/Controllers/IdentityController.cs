dc65d6c7933aed744df37995f03f8c70fc27b356
mkovachev <mkovachev6@gmail.com>
2020-04-15 21:20:04 +0300

    added RegisterUserRequestModel

--- Instagram.Server/Controllers/IdentityController.cs
+++ Instagram.Server/Controllers/IdentityController.cs
@@ -1,6 +1,13 @@
-﻿namespace Instagram.Server.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Instagram.Server.Controllers
 {
     public class IdentityController : ApiBaseController
     {
+        public async Task<IActionResult> Register()
+        {
+            var identity
+        }
     }
 }
---
+++ Instagram.Server/Models/Identity/RegisterUserRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Instagram.Server.Models.Identity
+{
+    public class RegisterUserRequestModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
