606cfa803fb46d6ab93f128a90f08e4bcc68656a
mkovachev <mkovachev6@gmail.com>
2020-04-17 18:52:24 +0300

    created

---
+++ Instagram.Server/Controllers/ItemsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Instagram.Server.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<int>> Create()
+        {
+
+        }
+    }
+}
