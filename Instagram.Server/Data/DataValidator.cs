49dc6adb422639062f3549cc4cc84951e68a0815
mkovachev <mkovachev6@gmail.com>
2020-04-17 18:58:25 +0300

    added

---
+++ Instagram.Server/Data/DataValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace Instagram.Server.Data
+{
+    public class DataValidator
+    {
+        public class Item
+        {
+            public const int MaxDescriptionLenght = 1000;
+        }
+    }
+}
