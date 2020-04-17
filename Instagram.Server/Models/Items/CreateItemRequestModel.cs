6e60490b446541d87b6252bf4bc703520c6d06fc
mkovachev <mkovachev6@gmail.com>
2020-04-17 18:52:10 +0300

    created

---
+++ Instagram.Server/Models/Items/CreateItemRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Instagram.Server.Models.Items
+{
+    public class CreateItemRequestModel
+    {
+        [MaxLength(1000)]
+        public int Description { get; set; }
+
+        //[Required]
+        public string ImageUrl { get; set; }
+    }
+}
