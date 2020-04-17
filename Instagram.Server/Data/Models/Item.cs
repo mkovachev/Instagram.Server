45f756bf7ad1c81d52365f074a0d47765fbedab2
mkovachev <mkovachev6@gmail.com>
2020-04-17 18:52:36 +0300

    added validations

---
+++ Instagram.Server/Data/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Instagram.Server.Data.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [MaxLength(1000)]
+        public int Description { get; set; }
+
+        //[Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
--- Instagram.Server/Data/Models/User.cs
+++ Instagram.Server/Data/Models/User.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace Instagram.Server.Data.Models
 {
     public class User : IdentityUser
     {
+        public IEnumerable<Item> Items { get; } = new HashSet<Item>();
     }
 }
