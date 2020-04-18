4dad2f4b6c8593fe992230d67caf2a354fc10ca3
mkovachev <mkovachev6@gmail.com>
2020-04-18 14:21:57 +0300

    created

---
+++ Instagram.Server/Features/Items/IItemsService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Instagram.Server.Features.Items
+{
+    public interface IItemsService
+    {
+        public Task<int> Create(string imageUrl, string description, string userId);
+
+        public Task<bool> Update(int id, string description, string userId);
+
+        public Task<bool> Delete(int id, string userId);
+
+        public Task<IEnumerable<ItemListingServiceModel>> ByUser(string userId);
+
+        public Task<ItemDetailsServiceModel> Details(int id);
+    }
+}
