9f7abe7b64accc85c682ec76a869e189c0908610
mkovachev <mkovachev6@gmail.com>
2020-04-18 11:46:27 +0300

    created

---
+++ Instagram.Server/Infrastructure/ConfigurationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Instagram.Server.Infrastructure
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetDefaultConnectionString(this IConfiguration configuration)
+            => configuration.GetConnectionString("DefaultConnection");
+    }
+
+}
