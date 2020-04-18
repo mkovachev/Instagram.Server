710ecb06a18475f2005669cfc7f140c900b69a8d
mkovachev <mkovachev6@gmail.com>
2020-04-18 14:24:26 +0300

    created

---
+++ Instagram.Server/Features/Items/ItemsService.cs
@@ -0,0 +1,35 @@
+﻿using Instagram.Server.Features.Items.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Instagram.Server.Features.Items
+{
+    public class ItemsService : IItemsService
+    {
+        public Task<IEnumerable<ItemListingServiceModel>> ByUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Create(string imageUrl, string description, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ItemDetailsServiceModel> Details(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(int id, string description, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
