1c7a93e940a1da2be4ad0dea1d247619670d183b
mkovachev <mkovachev6@gmail.com>
2020-04-18 11:22:49 +0300

    added ServiceCollectionExtension to refactor the startup

--- Instagram.Server/Startup.cs
+++ Instagram.Server/Startup.cs
@@ -1,16 +1,9 @@
-using Instagram.Server.Data;
-using Instagram.Server.Data.Models;
 using Instagram.Server.Infrastructure;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace Instagram.Server
 {
@@ -22,44 +15,12 @@ namespace Instagram.Server
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<InstagramDbContext>(options =>
-                options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
-
             services
-                .AddDefaultIdentity<User>(options =>
-                {
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequiredLength = 6;
-                })
-                .AddEntityFrameworkStores<InstagramDbContext>();
-
-            var appSettingsConfig = this.Configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingsConfig);
-
-            var appSettings = appSettingsConfig.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
-
-            services.AddControllers().AddNewtonsoftJson();
+                .ConnectToSqlServer(this.Configuration)
+                .AddIdentity()
+                .AddJwtAuthentication(this.Configuration)
+                .AddControllers()
+                .AddNewtonsoftJson();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
