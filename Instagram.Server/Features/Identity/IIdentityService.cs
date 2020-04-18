983f381580a60d38d620d0e5a39938fdbf94d95a
mkovachev <mkovachev6@gmail.com>
2020-04-18 14:17:55 +0300

    created

---
+++ Instagram.Server/Features/Identity/IIdentityService.cs
@@ -0,0 +1,7 @@
+﻿namespace Instagram.Server.Features.Identity
+{
+    public interface IIdentityService
+    {
+        string GenerateJwtToken(string userId, string userName, string secret);
+    }
+}
